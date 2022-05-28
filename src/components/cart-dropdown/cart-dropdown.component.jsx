b7b669d5a02184621223f6568803b97ed267593d
Keegan <leary.keegan@gmail.com>
2022-05-27 19:02:52 -0700

    can add products to cart as CartItems

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,20 @@
+import { useContext } from "react";
 import './cart-dropdown.styles.scss';
+import { CartContext } from "../../contexts/cart.context";
+import CartItem from "../cart-item/cart-item.component";
 import Button from "../button/button.component";
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+
   return (
     <div className='cart-dropdown-container'>
       <div className='cart-items'>
-        <Button>Go to Checkout</Button>
+        {cartItems.map((item) => {
+          return <CartItem key={item.id} cartItem={item} />
+        })}
       </div>
+      <Button>Go to Checkout</Button>
     </div>
   )
 }
